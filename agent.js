0cf628e6af2e80cab867520e99ed6852f58565d8
Abhishek Singh <jonu.1504@gmail.com>
2024-10-01 09:20:36 +0530

    use multiple llms based on task intensity

--- agent.js
+++ agent.js
@@ -1,10 +1,15 @@
-// import { ChatOllama } from "@langchain/ollama";
+import { ChatOllama } from "@langchain/ollama";
 import { ChatOpenAI } from "@langchain/openai";
 
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { PROMPT_TEMPLATE } from "./prompt/PROMPT_TEMPLATE.js";
 import client from "./pg.js";
+
+let ollama = new ChatOllama({
+    model: "llama3.1",
+    temperature: 0
+});
 
 let llm = new ChatOpenAI({
     model: "gpt-3.5-turbo",
@@ -20,7 +25,7 @@ async function execute_query_with_retry(sql_query, retry = 3) {
         const res = await client.query(sql_query);
         return JSON.stringify(res.rows);
     } catch (error) {
-        if (retry < 0) error.message;
+        if (retry < 0) return error.message;
         const correctionPrompt = `The following SQL query caused an error:\n
                             Query: ${sql_query}\n
                             Error: ${error.message}\n
@@ -31,24 +36,24 @@ async function execute_query_with_retry(sql_query, retry = 3) {
         let sqlChain = prompt.pipe(llm).pipe(new StringOutputParser());
 
         const sql_query2 = await sqlChain.invoke();
-        return execute_query(sql_query2, retry - 1);
+        return execute_query_with_retry(sql_query2, retry - 1);
     }
 }
 
-(async function () {
-    const question = process.argv[2];
-    const sql_query = await sqlChain.invoke({
-        question
-    });
-    console.log("Generated SQL query: \n", sql_query)
-    if (sql_query == 0) {
-        console.log("Sorry, I can't execute it.")
-        process.exit(0);
-    }
-    const result = await execute_query_with_retry(sql_query);
-    await client.end()
 
-    const PROMPT_TEMPLATE = `
+const question = process.argv[2];
+const sql_query = await sqlChain.invoke({
+    question
+});
+// console.log("Generated SQL query: \n", sql_query)
+if (sql_query == 0) {
+    console.log("Sorry, I can't execute it.")
+    process.exit(0);
+}
+const result = await execute_query_with_retry(sql_query);
+await client.end()
+
+const PROMPT_TEMPLATE2 = `
      Your are a helpful AI assistance. Answer based on below context.
        
      Question:  ${question}
@@ -58,8 +63,6 @@ async function execute_query_with_retry(sql_query, retry = 3) {
      Answer:
     `;
 
-    const prompt = ChatPromptTemplate.fromTemplate(PROMPT_TEMPLATE);
-    let sqlChain2 = prompt.pipe(llm).pipe(new StringOutputParser());
-    console.log(await sqlChain2.invoke({ result }))
-
-})()
+const prompt2 = ChatPromptTemplate.fromTemplate(PROMPT_TEMPLATE2);
+let sqlChain2 = prompt2.pipe(ollama).pipe(new StringOutputParser());
+console.log(await sqlChain2.invoke({ result }))
--- pg.js
+++ pg.js
@@ -4,3 +4,11 @@ const client = new Client("pg://mobilesync:mobilesync@localhost/mobilesync")
 await client.connect()
 
 export default client;
+
+export const get_Schema = () => {
+    // SELECT table_name, column_name, data_type, is_nullable
+    // FROM information_schema.columns
+    // WHERE table_schema = 'public'
+    // ORDER BY table_name, ordinal_position;
+    
+}
